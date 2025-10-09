84dd590426f3867e6915ae7dc2d6dac3b76a3775
Krisha2000 <krishasomapura@gmail.com>
2025-10-09 20:12:39 +0530

    Triggering CI/CD pipeline

--- feature_repo/data_sources.py
+++ feature_repo/data_sources.py
@@ -1,14 +1,27 @@
-from datetime import timedelta
+#from datetime import timedelta
+#from feast import FileSource
+#
+## The Prudential dataset does not have a real timestamp column.
+## In a real-world scenario, this would be the application submission time.
+## We'll use a placeholder for now, but Feast requires it to be defined.
+#applicant_source = FileSource(
+#    name="applicant_data_source",
+#    path="feature_repo/data/train.parquet", # We will create this parquet file in the next step
+#    timestamp_field="event_timestamp",
+#    created_timestamp_column="created_timestamp",
+#    description="A table of historical applicant data for risk assessment",
+#)
+#
+#
+from pathlib import Path
 from feast import FileSource
 
-# The Prudential dataset does not have a real timestamp column.
-# In a real-world scenario, this would be the application submission time.
-# We'll use a placeholder for now, but Feast requires it to be defined.
+file_path = str(Path(__file__).resolve().parent / "data/train.parquet")
+
 applicant_source = FileSource(
     name="applicant_data_source",
-    path="feature_repo/data/train.parquet", # We will create this parquet file in the next step
+    path=file_path,
     timestamp_field="event_timestamp",
     created_timestamp_column="created_timestamp",
     description="A table of historical applicant data for risk assessment",
 )
-
